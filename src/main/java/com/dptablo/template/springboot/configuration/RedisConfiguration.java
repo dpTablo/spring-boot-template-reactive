af62811c93dc1e7d705a7f927f33d7a251bc0a50
dpTablo <leeyw2355@gmail.com>
2022-11-18 11:47:23 +0900

    redis 진행중

---
+++ src/main/java/com/dptablo/template/springboot/configuration/RedisConfiguration.java
@@ -0,0 +1,37 @@
+package com.dptablo.template.springboot.configuration;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory jedisConnectionFactory() {
+        var clientConfiguration = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
+
+        var configuration = new RedisStandaloneConfiguration(host, port);
+
+        return new LettuceConnectionFactory(configuration, clientConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
